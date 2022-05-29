c92261edaf1907fe9e2bd993e8df2ffac3a31503
matteoferla <matteo.ferla@gmail.com>
2022-05-29 09:26:12 +0100

    :pencil: pypi description issue fix

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 from setuptools import setup, find_packages
 from typing import List
 
@@ -10,11 +10,11 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 if os.path.exists(os.path.join(this_directory, 'README.md')):
     with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
         long_description = f.read()
-        description = long_description.split('\n')[1]  # the first line is the description after the title
+        description = long_description.split('\n')[1].strip()  # the first line is the description after the title
 else:
     long_description = '(See GitHub README.md)'
     description = "GitHub Gist are handy snippets, which are meant to be copy-pasted into one's code... "+\
-                  "but what if you could import them? This is the solution."
+                  "but what if you could import them? This is the solution.".strip()
 
 if os.path.exists(os.path.join(this_directory, 'requirements.txt')):
     with open(os.path.join(this_directory, 'requirements.txt'), encoding='utf-8') as fh:
@@ -29,7 +29,6 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     url='https://github.com/matteoferla/gist-import',
-    license='MIT',
     author='Matteo Ferla',
     author_email='matteo.ferla@gmail.com',
     classifiers=[  # https://pypi.org/classifiers/
@@ -41,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3.9',
     ],
     description=description,
-    long_description=__doc__,
+    long_description=long_description,
     long_description_content_type='text/markdown',
+    license='MIT',
 )
